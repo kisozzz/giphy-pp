4d2a434e2fab8c168cca37abdebad115e0e7eb01
qixi zhang <zqx0803@me.com>
2022-03-09 21:50:44 +0800

    search bar added

---
+++ src/components/app.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import SearchBar from "./search_bar";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+        </div>
+        <div className="right-scene">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
