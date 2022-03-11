7fe8e34887375d385b6c26aad4152810b7157dbc
qixi zhang <zqx0803@me.com>
2022-03-11 21:14:05 +0800

    done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
+import giphy from "giphy-api";
 import SearchBar from "./search_bar";
 import Gif from "./gif";
+import GifList from "./gif_list";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      gifs: [],
+      selectedGifId: "3ohuPBfMriRvo20Yjm",
+    };
+  }
+
+  search = (query) => {
+    giphy("rBh7t5G7jUcNMYUuj1hrC8gxWPFYcwAY").search({
+      q: query,
+      rating: 'g'
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
   }
 
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search} />
           <div className="selected-gif">
-            <Gif id="3ohuPBfMriRvo20Yjm" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
       </div>
     );
   }
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -6,9 +6,13 @@ class SearchBar extends Component {
     this.state = {};
   }
 
+  handleChange = (event) => {
+    this.props.search(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search" />
+      <input type="text" className="form-control form-search" onChange={this.handleChange} />
     );
   }
 }
